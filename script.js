1e9cefd6e644966d9bca47beb83ac9b06d75f941
Donnahue George <Donnahuegjr@gmail.com>
2020-12-09 11:56:46 -0500

    Displaying timeblocks through Jquery

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-// var currentDayEl = $("#currentDay")
+//variables
+var currentDayEl = $("#currentDay")
+var containerEl = $(".container")
 var now = luxon.DateTime.local(2020, 5, 15, 8, 35)
+var dt = luxon.DateTime.local(2017, 5, 15, 8, 30);
 
 console.log(now)
+
+// current day
+
+
+//make timeblocks
+for (var i = 9; i < 19; i++) {
+    var newTimeBlock = $("<div>")
+    newTimeBlock.attr("class", "row col-8 description")
+    newTimeBlock.attr("id", "description")
+    newTimeBlock.attr("id", "time" + i)
+    containerEl.append(newTimeBlock)
+}
+console.log(dt.toLocaleString(DateTime.DATE_SHORT))
+
+
+
+//css timeblock
+
+
+
+//local storage
