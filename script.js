312469b7d43632f4f9f9b616fa1def5b906b6742
Donnahue George <Donnahuegjr@gmail.com>
2020-12-09 12:16:05 -0500

    added saved button

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ console.log(now)
 for (var i = 9; i < 19; i++) {
     var newTimeBlock = $("<div>")
     newTimeBlock.attr("class", "row")
-    newTimeBlock.attr("id", "time" + i)
+    newTimeBlock.attr("id", "time-block time" + i)
     containerEl.append(newTimeBlock)
 
     var newHour = $("<aside>")
@@ -27,6 +27,12 @@ for (var i = 9; i < 19; i++) {
     textBox.attr("id", "textTime" + i)
     textBox.val("Today's Plans are...")
     newTimeBlock.append(textBox)
+
+    var save = $("<button>")
+    save.attr("class", "saveBtn col-2")
+    save.attr("id", "savebutton" + i)
+    save.html("<i class= 'fas fa-save'\> <br> Save<\i>")
+    newTimeBlock.append(save)
 
 }
 console.log(dt.toLocaleString(DateTime.DATE_SHORT))
