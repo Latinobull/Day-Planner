8e9245508543a0ad2fe5d9553a89cc4e159472cf
Donnahue George <Donnahuegjr@gmail.com>
2020-12-10 18:04:39 -0500

    removed unneccessary code

--- script.js
+++ script.js
@@ -12,25 +12,21 @@ var currentHour = dt.hour
 for (var i = 9; i < 19; i++) {
     var newTimeBlock = $("<div>")
     newTimeBlock.attr("class", "row")
-    newTimeBlock.attr("id", "time-block time" + i)
+    newTimeBlock.attr("id", "time-block")
     containerEl.append(newTimeBlock)
 
     var newHour = $("<aside>")
     newHour.attr("class", "hour col-2")
-    newHour.attr("id", "hour")
-    newHour.attr("value", i)
     newHour.text(i)
     newTimeBlock.append(newHour)
 
     var textBox = $("<textarea>")
     textBox.attr("class", "description col-8")
-    textBox.attr("id", "textTime" + i)
     textBox.attr("placeholder","Today's Plans are...")
     newTimeBlock.append(textBox)
 
     var save = $("<button>")
     save.attr("class", "saveBtn col-2")
-    save.attr("id", "savebutton" + i)
     save.html("<i class= 'fas fa-save'> <br> Save</i>")
     newTimeBlock.append(save)
 
